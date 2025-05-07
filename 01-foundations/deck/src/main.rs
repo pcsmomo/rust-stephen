fbf22d324e4c267a70e846ea7bcc3176027e002e
Noah Kim <dongwoo_333@hanmail.net>
2025-05-07 14:42:05 +1000

    9. Arrays vs Vectors

--- 01-foundations/deck/src/main.rs
+++ 01-foundations/deck/src/main.rs
@@ -5,6 +5,18 @@ struct Deck {
 }
 
 fn main() {
+    let suits = vec!["Hearts", "Diamonds", "Clubs", "Spades"];
+    let values = vec!["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+    let cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
     let deck: Deck = Deck { cards: vec![] };
     // let deck: Deck = Deck { cards: Vec::new() }; // same as above
 
