ce11b3ee7f15d255ebcdf54e376e96eb49a6ef8a
Noah Kim <dongwoo_333@hanmail.net>
2025-05-08 17:08:03 +1000

    26. Exercise Overview

---
+++ 03-rust-memory-system/bank/src/example.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+fn main() {
+    let account = Account::new(1, String::from("me"));
+
+    // TODO: Write and call a function that will *take ownership* of the Account
+    // value, print it, and return nothing
+
+    // Question: Will you be able to call the function twice with the 'account'
+    // variable?
+}
