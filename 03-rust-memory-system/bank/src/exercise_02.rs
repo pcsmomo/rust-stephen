d445098590f4ed8d390f8ab3b64eadf881040dc4
Noah Kim <dongwoo_333@hanmail.net>
2025-05-08 17:11:38 +1000

    28. Another Quick Exercise

---
+++ 03-rust-memory-system/bank/src/exercise_02.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+fn print_bank(bank: Bank) {
+    println!("{:#?}", bank);
+}
+
+fn main() {
+    let bank = Bank::new();
+
+    // TODO: Write and call a function that will *take ownership* of
+    // the Banks's "accounts" field, print it, and return nothing
+
+    // Once you've finished the to-do, uncomment the 'print_bank' call below
+    // When your function + print_bank run, do you think you'll end up getting
+    // an error?
+    // If so, what error do you think you'd see?
+
+    // UNCOMMENT THIS:
+    // print_bank(bank);
+}
