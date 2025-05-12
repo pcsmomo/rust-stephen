efcb7ecf84c90d6a11b9d4fff216b8d5fd372dfa
Noah Kim <dongwoo_333@hanmail.net>
2025-05-12 17:40:34 +1000

    47. Declaring Enum Values

--- 05-enums/media/src/main.rs
+++ 05-enums/media/src/main.rs
@@ -14,6 +14,16 @@ fn main() {
         title: String::from("The Great Gatsby"),
         // title: "The Great Gatsby".to_string(),
     };
+    let good_movie = Media::Movie {
+        title: String::from("The Dark Knight"),
+        director: String::from("Christopher Nolan"),
+    };
+    let bad_book = Media::Book {
+        title: String::from("Bad Book"),
+        author: String::from("Bad Author"),
+    };
 
     print_media(audiobook);
+    print_media(good_movie);
+    print_media(bad_book);
 }
