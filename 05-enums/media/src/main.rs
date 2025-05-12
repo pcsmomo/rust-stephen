f586e8226186e00cf6f4df50b343bad6f2ab55e9
Noah Kim <dongwoo_333@hanmail.net>
2025-05-13 07:10:50 +1000

    52. Unlabeled Fields

--- 05-enums/media/src/main.rs
+++ 05-enums/media/src/main.rs
@@ -3,6 +3,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audio { title: String },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
@@ -19,9 +21,21 @@ impl Media {
 
         // Using `match` keyword
         match self {
-            Media::Book { title, author } => format!("Book: {} by {}", title, author),
-            Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
-            Media::Audio { title } => format!("Audiobook: {}", title),
+            Media::Book { title, author } => {
+                format!("Book: {} by {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {} by {}", title, director)
+            }
+            Media::Audio { title } => {
+                format!("Audiobook: {}", title)
+            }
+            Media::Podcast(episode_number) => {
+                format!("Podcast episode: {}", episode_number)
+            }
+            Media::Placeholder => {
+                format!("Placeholder")
+            }
         }
     }
 }
@@ -58,12 +72,16 @@ fn main() {
         title: String::from("Bad Book"),
         author: String::from("Bad Author"),
     };
+    let podcast = Media::Podcast(7);
+    let placeholder = Media::Placeholder;
 
     let mut catalog = Catalog::new();
 
     catalog.add(audiobook);
     catalog.add(good_movie);
     catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
 
     println!("{:#?}", catalog);
 }
