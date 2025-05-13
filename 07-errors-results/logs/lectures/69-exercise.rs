abca928e42e172ad7229abde87daca319977f014
Noah Kim <dongwoo_333@hanmail.net>
2025-05-13 13:36:53 +1000

    70. Exercise Solution

--- 07-errors-results/logs/lectures/69-exercise.rs
+++ 07-errors-results/logs/lectures/69-exercise.rs
@@ -1,13 +1,15 @@
 use std::io::Error;
 
 // Todo: add in a return type
-fn validate_ingredients(ingredients: &Vec<String>) {
+fn validate_ingredients(ingredients: &Vec<String>) -> Result<(), Error> {
     if ingredients.len() > 3 {
         // Todo: make it clear that this is an error!
+        Err(Error::other("too many ingredients"))
     } else {
         // Todo: make it clear that the ingredidents passed
         // validation. Note that we don't really have any value
         // to include in the 'Ok' here...
+        Ok(())
     }
 }
 
@@ -22,5 +24,8 @@ fn main() {
     // Todo: validation is an operation that might succeed or fail
     // Print out a success or fail message based on whether
     // it passes validation
-    validate_ingredients(&ingredients);
+    match validate_ingredients(&ingredients) {
+        Ok(..) => println!("Ingredients are good to go"),
+        Err(error_message) => panic!("Failed validation: {}", error_message),
+    }
 }
