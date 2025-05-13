1d1fc797e1b67abb7babb4b1302151357fabcbaa
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 09:06:13 +1000

    79. Writing Data to a File

--- 07-errors-results/logs/src/main.rs
+++ 07-errors-results/logs/src/main.rs
@@ -1,13 +1,30 @@
 use std::fs;
 
-fn extract_error(text: &str) -> Vec<String> {
+// Downside:
+// results leaves in heap memory and if the text size is huge, it will take a lot of memory
+
+// fn extract_error_and_return(text: &str) -> Vec<String> {
+//     let split_text = text.split("\n");
+
+//     let mut results = vec![];
+
+//     for line in split_text {
+//         if line.starts_with("ERROR") {
+//             results.push(line.to_string());
+//         }
+//     }
+
+//     results
+// }
+
+fn extract_error_and_return(text: &str) -> Vec<&str> {
     let split_text = text.split("\n");
 
     let mut results = vec![];
 
     for line in split_text {
         if line.starts_with("ERROR") {
-            results.push(line.to_string());
+            results.push(line);
         }
     }
 
@@ -15,15 +32,16 @@ fn extract_error(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    let mut error_logs = vec![];
+    // let mut error_logs = vec![];
 
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => {
-            // let local_error_logs = extract_error(text_that_was_read.as_str());
-            // println!("{:#?}", local_error_logs);
-            error_logs = extract_error(text_that_was_read.as_str());
+            // error_logs = extract_error_and_return(text_that_was_read.as_str());
 
-            match fs::write("errors.txt", error_logs.join("\n")) {
+            let local_error_logs = extract_error_and_return(text_that_was_read.as_str());
+            println!("{:#?}", local_error_logs);
+
+            match fs::write("errors.txt", local_error_logs.join("\n")) {
                 Ok(_) => println!("Wrote errors.txt"),
                 Err(why_this_failed) => {
                     panic!("Failed to write to file: {}", why_this_failed);
@@ -35,5 +53,5 @@ fn main() {
         }
     }
 
-    println!("{:#?}", error_logs);
+    // println!("{:#?}", error_logs);
 }
