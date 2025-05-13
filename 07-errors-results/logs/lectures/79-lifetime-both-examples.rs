7b546c2c120c3c5d849db6bcba661a2a65c5a9e2
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 09:13:01 +1000

    80. Alternatives to Nested Matches

---
+++ 07-errors-results/logs/lectures/79-lifetime-both-examples.rs
@@ -0,0 +1,57 @@
+use std::fs;
+
+// Downside:
+// results leaves in heap memory and if the text size is huge, it will take a lot of memory
+
+// fn extract_error_and_return(text: &str) -> Vec<String> {
+//     let split_text = text.split("\n");
+
+//     let mut results = vec![];
+
+//     for line in split_text {
+//         if line.starts_with("ERROR") {
+//             results.push(line.to_string());
+//         }
+//     }
+
+//     results
+// }
+
+fn extract_error(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn main() {
+    // let mut error_logs = vec![];
+
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => {
+            // error_logs = extract_error_and_return(text_that_was_read.as_str());
+
+            let local_error_logs = extract_error(text_that_was_read.as_str());
+            println!("{:#?}", local_error_logs);
+
+            match fs::write("errors.txt", local_error_logs.join("\n")) {
+                Ok(_) => println!("Wrote errors.txt"),
+                Err(why_this_failed) => {
+                    panic!("Failed to write to file: {}", why_this_failed);
+                }
+            }
+        }
+        Err(why_this_failed) => {
+            panic!("Failed to read file: {}", why_this_failed);
+        }
+    }
+
+    // println!("{:#?}", error_logs);
+}
--- 07-errors-results/logs/src/main.rs
+++ 07-errors-results/logs/src/main.rs
@@ -17,7 +17,7 @@ use std::fs;
 //     results
 // }
 
-fn extract_error_and_return(text: &str) -> Vec<&str> {
+fn extract_error(text: &str) -> Vec<&str> {
     let split_text = text.split("\n");
 
     let mut results = vec![];
@@ -32,26 +32,31 @@ fn extract_error_and_return(text: &str) -> Vec<&str> {
 }
 
 fn main() {
-    // let mut error_logs = vec![];
+    // constants
+    const FILE_NAME: &str = "logs.txt";
 
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            // error_logs = extract_error_and_return(text_that_was_read.as_str());
+    // 2. Short version - using `expect`
+    let text = fs::read_to_string(FILE_NAME).expect(&format!("Failed to read {}", FILE_NAME));
+    let error_logs = extract_error(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n")).expect("Failed to write to file");
 
-            let local_error_logs = extract_error_and_return(text_that_was_read.as_str());
-            println!("{:#?}", local_error_logs);
+    // 1. Long version - using match
+    // match fs::read_to_string(FILE_NAME) {
+    //     Ok(text_that_was_read) => {
+    //         // error_logs = extract_error_and_return(text_that_was_read.as_str());
 
-            match fs::write("errors.txt", local_error_logs.join("\n")) {
-                Ok(_) => println!("Wrote errors.txt"),
-                Err(why_this_failed) => {
-                    panic!("Failed to write to file: {}", why_this_failed);
-                }
-            }
-        }
-        Err(why_this_failed) => {
-            panic!("Failed to read file: {}", why_this_failed);
-        }
-    }
+    //         let local_error_logs = extract_error(text_that_was_read.as_str());
+    //         println!("{:#?}", local_error_logs);
 
-    // println!("{:#?}", error_logs);
+    //         match fs::write("errors.txt", local_error_logs.join("\n")) {
+    //             Ok(_) => println!("Wrote errors.txt"),
+    //             Err(why_this_failed) => {
+    //                 panic!("Failed to write to file: {}", why_this_failed);
+    //             }
+    //         }
+    //     }
+    //     Err(why_this_failed) => {
+    //         panic!("Failed to read file: {}", why_this_failed);
+    //     }
+    // }
 }
