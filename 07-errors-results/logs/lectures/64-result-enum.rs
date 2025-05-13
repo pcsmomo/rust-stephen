2dcfb417f91c4ddde8693680a3a824a8ae3bb53b
Noah Kim <dongwoo_333@hanmail.net>
2025-05-13 13:23:24 +1000

    67. Matching on Results

--- 07-errors-results/logs/lectures/64-result-enum.rs
+++ 07-errors-results/logs/lectures/64-result-enum.rs
@@ -1,14 +1,16 @@
 use std::io::Error;
 
-fn device(a: f64, b: f64) -> Result<f64, Error> {
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
     if b == 0.0 {
-        // error handling
-        Err(Error::other("cant divide by 0"))
+        Err(Error::other("can't divide by 0"))
     } else {
         Ok(a / b)
     }
 }
 
 fn main() {
-    println!("{:?}", device(10.0, 0.0));
+    match divide(5.0, 0.0) {
+        Ok(result_of_division) => println!("result: {}", result_of_division),
+        Err(what_went_wrong) => println!("error: {}", what_went_wrong),
+    }
 }
