29a5458a8bda5fcd7a4a64dc791c917371590c6e
Noah Kim <dongwoo_333@hanmail.net>
2025-05-13 13:41:23 +1000

    71. Using a Result When Reading Files

--- 07-errors-results/logs/src/main.rs
+++ 07-errors-results/logs/src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
-
-    println!("{:#?}", text);
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => println!("{}", text_that_was_read.len()),
+        Err(why_this_failed) => panic!("Failed to read file: {}", why_this_failed),
+    }
 }
