fc9e07d39ef330e62347e12065b50faa181f0ada
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 07:44:25 +1000

    76. Finding Error Logs

--- 07-errors-results/logs/lectures/72-tricky-strings.rs
+++ 07-errors-results/logs/lectures/72-tricky-strings.rs
@@ -1,9 +1,14 @@
-fn string_test(a: String, b: &String, c: &str) {}
+fn string_test(a: String, b: &String, c: &str) {
+    println!("{}", a);
+    println!("{}", b);
+    println!("{}", c);
+}
 
 fn main() {
     string_test(
         "red".to_string(),
         &String::from("red"),
         String::from("red").as_str(),
+        // &String::from("blue"), // automaticaly turn into &str
     );
 }
