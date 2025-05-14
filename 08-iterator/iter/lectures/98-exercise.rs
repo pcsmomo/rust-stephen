76e5fb29d52ed30f97cbbf9f5fefa39631070437
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 12:12:45 +1000

    98. Collect Excercise

---
+++ 08-iterator/iter/lectures/98-exercise.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+struct Account {
+    balance: i32,
+}
+
+fn main() {
+    let accounts: Vec<Account> = vec![Account { balance: 0 }, Account { balance: 10 }];
+
+    // TODO: getting a compiler error around the 'collect' call
+    // Remember: 'collect' can be used to gather values into
+    // many kinds of data structures. We have to explicitly
+    // tell collect what kind of structure we want by adding a
+    // type annotation
+    let balances = accounts.iter().map(|account| account.balance).collect();
+
+    println!("Balances: {:#?}", balances);
+}
