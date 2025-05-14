9a748ffd6ad5edd636faa56c7e2c0415b757fbc5
Noah Kim <dongwoo_333@hanmail.net>
2025-05-15 09:39:28 +1000

    113. Multiple Generic Types

--- 10-generics-traits/generics/src/main.rs
+++ 10-generics-traits/generics/src/main.rs
@@ -3,7 +3,7 @@ use num_traits::{Float, ToPrimitive};
 // First version - we can pass in BOTH f32 or f64
 // Second - we can pass in any type of numbers
 
-fn solve<T: Float>(a: T, b: T) -> f64 {
+fn solve<T: Float, U: Float>(a: T, b: U) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
@@ -12,7 +12,8 @@ fn solve<T: Float>(a: T, b: T) -> f64 {
 
 fn main() {
     let a: f32 = 3.0;
-    let b: f32 = 4.0;
+    let b: f64 = 4.0;
 
-    println!("{}", solve::<f32>(a, b));
+    // println!("{}", solve::<f32>(a, b));
+    println!("{}", solve(a, b));
 }
