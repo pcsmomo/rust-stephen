c37f5eed4cb6622390a3029bc2e1bf86b574cc78
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 15:04:34 +1000

    103. A Missing Annotation

--- 09-lifetimes/lifetimes/src/main.rs
+++ 09-lifetimes/lifetimes/src/main.rs
@@ -1,3 +1,28 @@
+fn next_language(languages: &[String], current: &str) -> &str {
+    let mut found = false;
+
+    for lang in languages {
+        if found {
+            return lang;
+        }
+
+        if lang == current {
+            found = true;
+        }
+    }
+
+    languages.last().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let languages = vec![
+        String::from("rust"),
+        String::from("go"),
+        String::from("typescript"),
+        String::from("python"),
+    ];
+
+    let next = next_language(&languages, "go");
+
+    println!("{}", next);
 }
