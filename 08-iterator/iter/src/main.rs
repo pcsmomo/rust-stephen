84893feef69bdc7a3095ff5cc8622c2e100101ba
Noah Kim <dongwoo_333@hanmail.net>
2025-05-14 10:57:50 +1000

    89. Reminder on Ownership and Borrowing

--- 08-iterator/iter/src/main.rs
+++ 08-iterator/iter/src/main.rs
@@ -1,7 +1,3 @@
-// expect full vector
-// fn print_elements(elements: &Vec<String>) {
-
-// expect slice (full vector or part of vector)
 fn print_elements(elements: &[String]) {
     // for element in elements {
     //     println!("{}", element);
@@ -12,13 +8,15 @@ fn print_elements(elements: &[String]) {
         .for_each(|el| println!("{}", el));
 }
 
+fn shorten_strings(elements: &mut Vec<String>) {}
+
 fn main() {
-    let colors = vec![
+    let mut colors = vec![
         String::from("red"),
         String::from("green"),
         String::from("blue"),
     ];
 
-    // print_elements(&colors[1..3]);
-    print_elements(&colors);
+    // print_elements(&colors);
+    shorten_strings(&mut colors);
 }
