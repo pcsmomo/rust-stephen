da286fc475145ba143bb672a2940d8a2554b5ec8
Noah Kim <dongwoo_333@hanmail.net>
2025-05-15 09:28:32 +1000

    111. The Basics of Generics

--- 10-generics-traits/generics/src/main.rs
+++ 10-generics-traits/generics/src/main.rs
@@ -1,15 +1,18 @@
-use num_traits::ToPrimitive;
+use num_traits::{Float, ToPrimitive};
 
-fn solve(a: f64, b: f64) -> f64 {
-    (a.powi(2) + b.powi(2)).sqrt()
+// First version - we can pass in BOTH f32 or f64
+// Second - we can pass in any type of numbers
+
+fn solve<T: Float>(a: T, b: T) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
 fn main() {
     let a: f32 = 3.0;
-    let b: f64 = 4.0;
+    let b: f32 = 4.0;
 
-    // let a_f64 = a as f64;
-    let a_f64 = a.to_f64().unwrap();
-
-    println!("{}", solve(a_f64, b));
+    println!("{}", solve::<f32>(a, b));
 }
