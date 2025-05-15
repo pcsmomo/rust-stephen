b61ad371c40d3ef700b210be043c892e621a87d1
Noah Kim <dongwoo_333@hanmail.net>
2025-05-15 10:41:49 +1000

    feat(ray tracing): initial fill pixel and save it as png

--- 99-scicomp/ex_ray_tracing/src/main.rs
+++ 99-scicomp/ex_ray_tracing/src/main.rs
@@ -1,3 +1,19 @@
+use image::{Rgb, RgbImage};
+
+const RESULT_DIR: &str = "results";
+const RESULT_FILE_NAME: &str = "image.png";
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = RgbImage::new(100, 100);
+
+    let green = Rgb([0, 255, 0]);
+
+    for y in 0..100 {
+        for x in 0..100 {
+            img.put_pixel(x, y, green);
+        }
+    }
+
+    img.save(format!("{}/{}", RESULT_DIR, RESULT_FILE_NAME))
+        .unwrap();
 }
