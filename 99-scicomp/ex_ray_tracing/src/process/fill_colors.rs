980869483dd1d87c0dfed8cd2725ab4ed484aff4
Noah Kim <dongwoo_333@hanmail.net>
2025-05-15 11:11:56 +1000

    refactor: rename stuff

--- 99-scicomp/ex_ray_tracing/src/process/fill_colors.rs
+++ 99-scicomp/ex_ray_tracing/src/process/fill_colors.rs
@@ -1,20 +1,5 @@
-//! Utilities for filling all pixels in an image with a specified color.
 use image::{Rgb, RgbImage};
 
-/// Fills the entire image with the given color.
-///
-/// # Arguments
-///
-/// * `img` - A mutable reference to the image to fill.
-/// * `color` - The color to fill the image with.
-///
-/// # Example
-/// ```
-/// use image::{Rgb, RgbImage};
-/// use crate::process::fill_pixel::fill_image;
-/// let mut img = RgbImage::new(100, 100);
-/// fill_image(&mut img, Rgb([0, 255, 0]));
-/// ```
 pub fn fill_one_color(img: &mut RgbImage, color: Rgb<u8>) {
     let (width, height) = img.dimensions();
 
