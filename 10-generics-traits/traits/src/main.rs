41903992b93a20a463183ff05ebe3759ee3a9041
Noah Kim <dongwoo_333@hanmail.net>
2025-05-21 20:10:25 +1000

    116. Building the Basket

--- 10-generics-traits/traits/src/main.rs
+++ 10-generics-traits/traits/src/main.rs
@@ -4,4 +4,10 @@ use basket::Basket;
 
 fn main() {
     let b1 = Basket::new(String::from("hi there"));
+    let b2 = Basket::new(10);
+    let b3 = Basket::new(true);
+
+    println!("b1: {:#?}", b1);
+    println!("b2: {:#?}", b2);
+    println!("b3: {:#?}", b3);
 }
