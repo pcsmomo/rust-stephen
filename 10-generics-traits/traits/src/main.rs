f84901d52624c32705e6efec8dffda4c9c2cf073
Noah Kim <dongwoo_333@hanmail.net>
2025-05-21 20:36:45 +1000

    120. Generic Trait Bounds (Final Lecture)

--- 10-generics-traits/traits/src/main.rs
+++ 10-generics-traits/traits/src/main.rs
@@ -6,21 +6,25 @@ use basket::Basket;
 use container::Container;
 use stack::Stack;
 
+fn add_string<T: Container<String>>(c: &mut T, s: String) {
+    c.put(s);
+}
+
 fn main() {
-    let b1 = Basket::new(String::from("hi there"));
+    let mut b1 = Basket::new(String::from("hi there"));
     let b2 = Basket::new(10);
     let b3 = Basket::new(true);
 
     let mut s1 = Stack::new(vec![String::from("hi there"), String::from("hi there 2")]);
-    let mut s2 = Stack::new(vec![1, 2, 3]);
+    let s2 = Stack::new(vec![1, 2, 3]);
 
-    s1.put(String::from("hi there 3"));
-    s2.put(4);
-
-    println!("s1: {:#?}", s1);
-    println!("s2: {:#?}", s2);
+    add_string(&mut b1, String::from("hi"));
+    add_string(&mut s1, String::from("hi"));
 
     println!("b1: {:#?}", b1);
     println!("b2: {:#?}", b2);
     println!("b3: {:#?}", b3);
+
+    println!("s1: {:#?}", s1);
+    println!("s2: {:#?}", s2);
 }
