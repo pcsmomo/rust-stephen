77b658b8acf4031619c46fd02f519c69a4d5e0f1
Noah Kim <dongwoo_333@hanmail.net>
2025-05-21 20:20:30 +1000

    118. More on Generic Structs

--- 10-generics-traits/traits/src/main.rs
+++ 10-generics-traits/traits/src/main.rs
@@ -1,11 +1,22 @@
 mod basket;
+mod stack;
 
 use basket::Basket;
+use stack::Stack;
 
 fn main() {
     let b1 = Basket::new(String::from("hi there"));
     let b2 = Basket::new(10);
     let b3 = Basket::new(true);
+
+    let mut s1 = Stack::new(vec![String::from("hi there"), String::from("hi there 2")]);
+    let mut s2 = Stack::new(vec![1, 2, 3]);
+
+    s1.put(String::from("hi there 3"));
+    s2.put(4);
+
+    println!("s1: {:#?}", s1);
+    println!("s2: {:#?}", s2);
 
     println!("b1: {:#?}", b1);
     println!("b2: {:#?}", b2);
